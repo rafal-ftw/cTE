f5a182621a4ab0cf88e58ef8059d539845d3874a
Rafal Draws <drawsr@emea.roche.com>
2022-08-01 20:59:06 +0200

    working dir changes

--- scraping/bin/webdriverSettings.py
+++ scraping/bin/webdriverSettings.py
@@ -3,10 +3,10 @@ from selenium import webdriver
 
 def initiateWebDriverOptions():
     
-    adblock = f"{os.getcwd()}\\bin\\adblock-v1.12.4.crx"
+    adblock = f"{os.getcwd()}/scraping/bin/adblock-v1.12.4.crx"
 
     option = webdriver.ChromeOptions()
-    #option.add_argument("--headless")
+    option.add_argument("--headless")
     option.add_extension(adblock)
     
     return option
