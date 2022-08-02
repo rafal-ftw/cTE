37adc45e0dbb8864f803a5785e26d5af80e3babf
Rafal Draws <drawsr@emea.roche.com>
2022-08-02 20:08:38 +0200

    navigation refactor, issue#7 done

--- scraping/bin/scraping.py
+++ scraping/bin/scraping.py
@@ -1,18 +1,61 @@
+import requests
 from bs4 import BeautifulSoup
 
-def scrapModelsFromCurrentSite_AndSendRequestToDatabase(source):
+def scrapModelsFromCurrentSite_AndSendRequestToDatabase(source, currentSearchParameters):
 
 
     soup = BeautifulSoup(source, 'html.parser')
     articles = soup.find_all('article', attrs = {'data-testid' : 'listing-ad'})
     
     print("scraping started")
+    print(currentSearchParameters)
 
     for article in articles:
-        link = article.find('a').get('href')
-        uniqueId = link[-13:-5]
-        print(link)
-        print(uniqueId)
 
+        requestBody = dict({
+            "MANUFACTURER":"",
+            "ARTICLEID":"",
+            "SERIES":"",
+            "GEN":"",
+            "YEAR":"",
+            "MILEAGE":"",
+            "ENGSIZE":"",
+            "FUELTYPE":"",
+            "PRICE":"",
+            "ARTCREATION":""
+        })
+
+        yearMileageEngineYear = []
         for li in article.find('ul').find_all('li'):
-            print(li.get_text())
+            yearMileageEngineYear.append(li.get_text())
+        
+        requestBody     ["YEAR"] = str(yearMileageEngineYear[0])
+        requestBody     ["MILEAGE"] = yearMileageEngineYear[1]
+        requestBody     ["ENGSIZE"] = yearMileageEngineYear[2]
+        requestBody     ["FUELTYPE"] = yearMileageEngineYear[3]
+
+        if len(currentSearchParameters) == 3:
+            requestBody ["MANUFACTURER"] = currentSearchParameters[0].upper()
+            requestBody ["SERIES"] = currentSearchParameters[1].upper()
+            requestBody ["GEN"] = currentSearchParameters[2].upper()
+        else:
+            requestBody ["MANUFACTURER"] = currentSearchParameters[0]
+            requestBody ["SERIES"]       = currentSearchParameters[1]
+
+        uniqueId = article.find('a').get('href')[-13:-5]
+        requestBody     ['ARTICLEID'] = uniqueId
+
+        price = article.find_all('div')[3].find('span').get_text()
+        requestBody     ["PRICE"]            = price
+
+        print(requestBody)
+
+        #TODO sendRequest()
+
+
+
+def sendRequest(url, requestBody):
+    requests.post(url, 
+        json = {'body' : requestBody},
+        headers = {}
+        )
